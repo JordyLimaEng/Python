62a2251936344ffdcf92d1969c8d1889a29369d5
Jordy Allyson <jordy.sousa@eng.ci.ufpb.br>
2019-04-20 00:42:45 -0300

    Update server_local.py

--- server_local.py
+++ server_local.py
@@ -10,8 +10,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(b'{json,pattern,example}')
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
