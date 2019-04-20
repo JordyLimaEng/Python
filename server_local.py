e817a32e665ed023d9cc12796f1df13770e873b0
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2019-04-20 00:41:42 -0300

    Server Simples
    Uma base para um server local para acessar dados que possam ser úteis em alguma implementação de projeto.

---
+++ server_local.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(b'{json,pattern,example}')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(b'{json,pattern,example}')
+        
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
