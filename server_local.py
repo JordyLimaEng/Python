39e0f0ed80d1d087c2da11edce4986a639928328
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2019-04-20 01:42:34 -0300

    Update server_local.py

--- server_local.py
+++ server_local.py
@@ -1,19 +1,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
+import json
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        arq = open('Config.txt', 'r')
+        texto = arq.read()
+        arq.close()
+        json_str = json.dumps(texto)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write(b'{json,pattern,example}')
+        self.wfile.write(json_str.encode(encoding='utf_8'))
 
     def do_POST(self):
+        arq = open('Config.txt', 'r')
+        texto = arq.read()
+        arq.close()
+        json_str = json.dumps(texto)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write(b'{json,pattern,example}')
+        self.wfile.write(json_str.encode(encoding='utf_8'))
         
 
 httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
