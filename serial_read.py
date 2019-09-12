43deb315c01261dab5ec4f57b0b39ed3302b76fb
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2019-09-11 22:29:18 -0300

    Code for Serial read from Arduino

--- serial_read.py
+++ serial_read.py
@@ -1,19 +1,8 @@
 import serial
-import time
-import matplotlib.pyplot as plt
 
-ser = serial.Serial('COM13', 115200, timeout=0)
-Valores = []
-
-plt.axis([0, 300, 0, 1000000])
-plt.xlabel("in LUX")
-plt.ylabel("in OHM")
-
+ser = serial.Serial('COM3', 115200, timeout=0) #Selecionar a porta correta, verificar antes em que porta está conectado o arduino
 fg = False
 fg_y = False
-cont = 0
-x=0
-y=0
 Valores = []
 
 f= open("lux_res.txt","w+")
@@ -22,14 +11,12 @@ while 1:
    data = ser.readline().decode('utf-8').strip()   
    if data != "":
        Valores.append(data)
-       print(Valores)
+       #print(Valores)
        
        if fg == False:
-           f.write(data + ", ") #valores em y (res)
+           f.write(data + "; ") #valores em y (res)
            fg = True
        else:
            f.write(data + ";\n") #valores em x (lux)
            fg = False
        
-       #time.sleep(0.2)
-       #cont = cont + 1
